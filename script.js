9bbc5f7eb5a56bcde4050a0d75ccdfe71b9d1f8f
Hamza-Asif-HA2 <hamzaasif0726@gmail.com>
2024-08-02 02:37:41 +0500

    Project 1

---
+++ script.js
@@ -0,0 +1,63 @@
+let arr = [
+    {name: "Petals of roses", image: "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Animals of town", image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "the crowd of city", image: "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+]
+
+function showTheCards(array){
+    var clutter = "";
+    array.forEach(function(obj){
+        clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image">
+        <div class="caption">Lorem ipsum </div>
+    </div>`;
+    })
+
+    document.querySelector(".container")
+    .innerHTML = clutter;
+}
+
+function handleSearchFunctionality(){
+    var input = document.querySelector("#searchinput");
+    
+
+    input
+    .addEventListener("focus", function(){
+        document.querySelector(".overlay").style.display = "block";
+        // document.querySelector(".searchdata").style.display = "block";
+    })
+
+    input
+    .addEventListener("blur", function(){
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelector(".searchdata").style.display = "none";
+    })
+
+    input
+    .addEventListener("input", function(){
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().includes(input.value));
+        //Here This Line will show only search related posts
+        showTheCards(filteredArray)
+
+        //This code displays search-history-like stuff OR related seaches
+        var clutter = "";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+        })
+        //this line display(unhide) the realted search div
+        document.querySelector(".searchdata").style.display = "block";
+        //this line adds the related search data in search div
+        document.querySelector(".searchdata").innerHTML = clutter;
+    })
+}
+
+handleSearchFunctionality();
+//this functions displays all posts(objects)
+showTheCards(arr);
